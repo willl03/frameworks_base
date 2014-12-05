07e702a107b09a7bc2002ef21c7d735437531242
Steve Kondik <steve@cyngn.com>
2014-12-05 08:31:29 -0800

    SystemUI: Forward-port notification counters [1/2]
    Change-Id: Ia8609c56a523906404a9155cea6987f40c10f30d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3616,6 +3616,12 @@ public final class Settings {
         public static final String VOLUME_ROCKER_MUSIC_CONTROLS = "volume_rocker_music_controls";
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
