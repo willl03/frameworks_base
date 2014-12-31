6e017b33d2619449fec7d0ba4dae531f42c536d9
Matt Mower <mowerm@gmail.com>
2014-12-31 15:02:58 -0600

    Keyguard: Forward port lockscreen quick unlock (1/2)
    NOTICE: For the love of Android, please don't use this commit in any
    builds shared with the community. It almost certainly mishandles
    multiple users and bypasses important security mechanisms.
    
    * PasswordTextView QuickUnlock listener functions and method thanks to
      ParanoidAndroid project:
        Keyguard: Implement quick unlock feature [1/2]
        BigBrother1984 <carlosavignano@aospa.co>
        I0b3d92b91f72020d327a7f2f184c2fbc5519a5df
    
    Change-Id: Iaa140723ffb037062d565127257a7cb60526f25e

--- packages/Keyguard/src/com/android/keyguard/KeyguardPINView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardPINView.java
@@ -19,6 +19,7 @@ package com.android.keyguard;
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.RenderNode;
 import android.view.RenderNodeAnimator;
@@ -26,6 +27,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 
+import com.android.internal.widget.LockPatternUtils.RequestThrottledException;
+import com.android.keyguard.PasswordTextView.QuickUnlockListener;
 import com.android.settingslib.animation.AppearAnimationUtils;
 import com.android.settingslib.animation.DisappearAnimationUtils;
 
@@ -46,6 +49,8 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
     private int mDisappearYTranslation;
     private View[][] mViews;
     private final KeyguardUpdateMonitor mKeyguardUpdateMonitor;
+
+    private final int userId = KeyguardUpdateMonitor.getCurrentUser();
 
     public KeyguardPINView(Context context) {
         this(context, null);
@@ -111,6 +116,19 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
                 new View[]{
                         null, mEcaView, null
                 }};
+
+        boolean quickUnlock = (Settings.System.getInt(getContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_QUICK_UNLOCK_CONTROL, 0) == 1);
+     
+        if (quickUnlock) {
+            mPasswordEntry.setQuickUnlockListener(new QuickUnlockListener() {
+                public void onValidateQuickUnlock(String password) {
+                    validateQuickUnlock(password);
+                }
+            });
+        } else {
+            mPasswordEntry.setQuickUnlockListener(null);
+        }
     }
 
     @Override
@@ -172,5 +190,24 @@ public class KeyguardPINView extends KeyguardPinBasedInputView {
     @Override
     public boolean hasOverlappingRendering() {
         return false;
+    }
+
+    private void validateQuickUnlock(String password) {
+        if (password != null) {
+            if (password.length() > MINIMUM_PASSWORD_LENGTH_BEFORE_REPORT
+                    && kpvCheckPassword(password)) {
+                mCallback.reportUnlockAttempt(true, userId);
+                mCallback.dismiss(true);
+                resetPasswordText(true);
+            }
+        }
+    }
+
+    private boolean kpvCheckPassword(String password) {
+        try {
+            return mLockPatternUtils.checkPassword(password, userId);
+        } catch (RequestThrottledException ex) {
+            return false;
+        }
     }
 }
