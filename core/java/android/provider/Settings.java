a5ad3313716aac06ab8a0a8caf4b6bf05e2a843d
mydongistiny <jaysonedson@gmail.com>
2016-09-12 13:26:03 -0700

    FWB: Disable suggestions [2/2]
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3622,6 +3622,12 @@ public final class Settings {
         public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
 
         /**
+         * Disable suggestions on the settings dashboard
+         * @hide
+         */
+        public static final String DISABLE_SETTINGS_SUGGESTIONS = "disable_settings_suggestions";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
